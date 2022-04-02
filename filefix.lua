d919cbdfc5fee1a980e662d5b5fface4bfcabbfb
mecwerks <mecwerks@gmail.com>
2022-04-02 15:03:50 -0400

    add fix for loading libs from local dir without packaging

--- filefix.lua
+++ filefix.lua
@@ -3,3 +3,11 @@ local APPDIR = os.getenv("APPDIR")
 if APPDIR then
     package.cpath = package.cpath .. ";" .. APPDIR .. "/usr/lib/?.so"
 end
+
+-- TODO: add macos support
+local sep = package.cpath:find("\\") and "\\" or "/"
+local ext = package.cpath:find("?.so") and ".so" or ".dll"
+local folder = love.system.getOS() == "Linux" and "linux-x86_64" or "win64"
+
+-- This is mainly to load shared libs in development
+package.cpath = package.cpath .. ";" .. love.filesystem.getWorkingDirectory() .. sep .. "libs" .. sep .. folder .. sep .. "?" .. ext
