64ff434e76d0a58ff771c4e198f49b4a2b065dc8
marxy <mecwerks@gmail.com>
2022-11-11 23:03:32 -0500

    Fixed loading game from external paths

--- filefix.lua
+++ filefix.lua
@@ -1,13 +1,20 @@
+-- TODO: add macos support
+local sep = package.cpath:find("\\") and "\\" or "/"
+local ext = package.cpath:find("?.so") and ".so" or ".dll"
+local folder = love.system.getOS() == "Linux" and "linux-x86_64" or "win64"
+
 -- Fixes loading shared libs on linux with AppImage
 local APPDIR = os.getenv("APPDIR")
 if APPDIR then
     package.cpath = package.cpath .. ";" .. APPDIR .. "/usr/lib/?.so"
 end
 
--- TODO: add macos support
-local sep = package.cpath:find("\\") and "\\" or "/"
-local ext = package.cpath:find("?.so") and ".so" or ".dll"
-local folder = love.system.getOS() == "Linux" and "linux-x86_64" or "win64"
+function addPath(path)
+    package.cpath = package.cpath .. ";" .. path .. sep .. "libs" .. sep .. folder .. sep .. "?" .. ext
+end
 
--- This is mainly to load shared libs in development
-package.cpath = package.cpath .. ";" .. love.filesystem.getWorkingDirectory() .. sep .. "libs" .. sep .. folder .. sep .. "?" .. ext
+addPath(love.filesystem.getWorkingDirectory())
+addPath(love.filesystem.getAppdataDirectory())
+addPath(love.filesystem.getUserDirectory())
+addPath(love.filesystem.getSourceBaseDirectory())
+addPath(arg[1])
